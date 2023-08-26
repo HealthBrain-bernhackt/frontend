a336c03bfed8587bf625161c3ae4875c3e68a0a4
BenEliasMartin <martinbenelias@gmail.com>
2023-08-26 12:07:54 +0200

    fancy types

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,9 @@
-import React from 'react'
+import { useState } from 'react'
 import { IonButton, IonContent, IonPage, IonTitle } from '@ionic/react'
 import HeadNav from '../components/HeadNav'
 import { PieChart } from 'react-minimal-pie-chart';
 import calendar from '../assets/calendar.png'
-
-const data = [
-    {
-        name: 'Medication A', value: 10, dosage: '80mg', times: [{ time: '8:00', taken: true }, { time: '12:30', taken: false }, { time: '16:00', taken: false }, { time: '16:00', taken: false }]
-    },
-    {
-        name: 'Medication B', value: 10, dosage: '200mg', times: [{ time: '8:00', taken: true }, { time: '20:00', taken: false }]
-    },
-
-]
+import { MedicationToday } from '../types/MedicationToday';
 
 
 const date = new Date();
@@ -22,28 +13,36 @@ const year = date.getFullYear();
 const hour = date.getHours();
 
 const returnNearestMedicationTime = (medication: any) => {
+
+
     const times = medication.times;
     const currentTime = new Date();
     let nearestTime: string | null = null;
-    let nearestIndex: number | null = null;
+    let smallestDifference: number | null = null;
+    let nearestIndex: number = -1;
 
     times.forEach((timeObject: any, index: number) => {
         if (timeObject.taken === true) {
             return; // Skip this time and continue with the next iteration
         }
 
-        if (!nearestTime || timeObject.time < nearestTime) {
+        const medicationTime = new Date(timeObject.time);
+        const timeDifference = Math.abs(currentTime.getTime() - medicationTime.getTime());
+
+        if (smallestDifference === null || timeDifference < smallestDifference) {
+            smallestDifference = timeDifference;
             nearestTime = timeObject.time;
             nearestIndex = index;
         }
     });
 
     if (nearestTime === null) {
-        return { timeString: 'No upcoming time', index: -1 };
+        return { timeString: null, index: -1 };
     }
 
     return { timeString: nearestTime, index: nearestIndex };
 };
+
 
 
 
@@ -62,6 +61,17 @@ const returnLatestTaken = (medication: any) => {
 
 
 export default function Home() {
+    const [data, setData] = useState(
+        new Array<MedicationToday>(
+            {
+                name: 'Medication A', value: 10, dosage: '80mg', times: [{ time: '8:00', taken: true }, { time: '12:30', taken: true }, { time: '16:00', taken: false }, { time: '20:00', taken: false }]
+            },
+            {
+                name: 'Medication B', value: 10, dosage: '200mg', times: [{ time: '8:00', taken: false }, { time: '20:00', taken: false }]
+            },
+        )
+    );
+
     return (
         <IonPage>
             <HeadNav back={false} />
@@ -89,32 +99,40 @@ export default function Home() {
 
                             return (
                                 <div key={index}>
-                                    <div className="mt-5">
-                                        <p className="font-bold text-lg">{nearestTime.timeString}</p>
-                                        <div className="grid-cols-2 grid rounded-md bg-gray-100 px-5 py-6">
-                                            <div>
-                                                <p className="font-bold text-lg">{medication.name}</p>
-                                                <p className="">{medication.dosage}</p>
-                                            </div>
-                                            <div className="flex justify-end items-center">
-                                                <input
-                                                    type="checkbox"
-                                                    className="w-6 h-6 border-2 border-gray-400 rounded-full transition-all checked:bg-blue-500 checked:border-blue-500"
-                                                    onChange={() => {
-                                                        // Check if nearestTime is not null
-                                                        if (nearestTime !== null) {
-                                                            const { index } = nearestTime;
+                                    {nearestTime.timeString != null &&
+                                        <div className="mt-5">
+                                            <p className="font-bold text-lg">{nearestTime.timeString}</p>
+                                            <div className="grid-cols-2 grid rounded-md bg-gray-100 px-5 py-6">
+                                                <div>
+                                                    <p className="font-bold text-lg">{medication.name}</p>
+                                                    <p className="">{medication.dosage}</p>
+                                                </div>
+                                                <div className="flex justify-end items-center">
+                                                    <input
+                                                        type="checkbox"
+                                                        className="w-6 h-6 border-2 border-gray-400 rounded-full transition-all checked:bg-blue-500 checked:border-blue-500"
+                                                        onChange={() => {
 
-                                                            // Update the 'taken' property if the index is valid
-                                                            if (index !== null && index !== -1 && data[index].times) {
-                                                                data[index].times[nearestTime.index].taken = true;
+                                                            // Check if nearestTime is not null
+                                                            if (nearestTime !== null) {
+
+                                                                console.log(index)
+                                                                console.log(data[index])
+                                                                // Update the 'taken' property if the index is valid
+                                                                if (index !== -1 && data[index].times) {
+                                                                    const updatedData = [...data]; // Create a copy of the data array
+                                                                    updatedData[index].times[nearestTime.index].taken = true;
+                                                                    setData(updatedData); // Update the state with the new data
+                                                                }
                                                             }
-                                                        }
-                                                    }}
-                                                />
+                                                        }}
+                                                    />
+
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
+                                    }
+
                                 </div>
                             );
                         })}
